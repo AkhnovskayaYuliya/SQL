810a2d623b56796e8f98e796909b1570d6fa5c4d
Akhnovskaya Yuliya <ahnovskaya.ju@mail.ru>
2023-10-19 22:03:32 +0300

    Change VerificationPage

--- src/test/java/ru/netology/SQL/pages/VerificationPage.java
+++ src/test/java/ru/netology/SQL/pages/VerificationPage.java
@@ -11,11 +11,17 @@ public class VerificationPage {
     private final SelenideElement code = $("[data-test-id=code] input");
     private final SelenideElement verificationButton = $("[data-test-id=action-verify]");
     private final SelenideElement errorMessage = $("[data-test-id=error-notification] .notification__content");
+
     public DashBoard validVerify(String verificationCode) {
-        code.setValue(verificationCode);
-        verificationButton.click();
+        setCode(verificationCode);
         return new DashBoard();
     }
+
+    public void setCode(String verificationCode) {
+        code.setValue(verificationCode);
+        verificationButton.click();
+    }
+
     public void errorNotification(String expectedText) {
         errorMessage.shouldHave(text(expectedText)).shouldBe(visible);
     }
